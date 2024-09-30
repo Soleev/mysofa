2dee417f18dc30cae795bc55e88e5742c0bd2ab7
Shokhrukh <shohruhhon@gmail.com>
2024-09-30 17:45:12 +0500

    add all pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/contacts', function () {return view('pages.contacts');});
+Route::get('/about', function () {return view('pages.about');});
+Route::get('/faqs', function () {return view('pages.faqs');});
+Route::get('/about-me', function () {return view('pages.about-me');});
+Route::get('/shop-category', function () {return view('pages.shop-category');});
