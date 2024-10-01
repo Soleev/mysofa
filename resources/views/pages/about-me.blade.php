9e0e2177ed2cfb97a526cdedf9d8e985d8bc7edf
Shokhrukh <shohruhhon@gmail.com>
2024-10-02 02:49:33 +0500

    edit about me

--- resources/views/pages/about-me.blade.php
+++ resources/views/pages/about-me.blade.php
@@ -6,16 +6,13 @@
         <div class="container">
             <div class="ps-about-me__info">
                 <h3>Алена</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed fermentum nibh, vel aliquet
-                    massa. Etiam in magna id risus lacinia luctus eget eu est.</p>
-                <p>Suspendisse leo sodales varius leo elementum vestibulum a elit at ultrices a mi a praesent enim
-                    libero ultrices a amet suscipit.</p>
-                <ul class="ps-list--social-2 mt-20">
-                    <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                    <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-                </ul>
+                <p>Компания мягкой мебели My Sofa, была основана в 2017г., создатель которой милая девушка.
+                    За 7 лет существования, команда пополнилась талантливыми сотрудниками, из маленького подмастерья компания выросла в большое стабильное производство.
+                </p>
+                <p>
+                    Наш дружный коллектив стремится удовлетворить запросы потребителей
+                    Мы стараемся внедрить современные, стильные фасоны и модели мягкой мебели, отвечаем за качество и своевременное исполнение заказов.
+                </p>
             </div>
         </div>
     </div>
--- resources/views/pages/shop-category.blade.php
+++ resources/views/pages/shop-category.blade.php
@@ -9,8 +9,10 @@
                 <li><a href="/">Главная</a></li>
                 <li class="active">Каталог</li>
             </ol>
+            <h1>Каталог Мебели MySofa: Широкий Выбор для Любого Интерьера</h1>
         </div>
     </div>
+
     <div class="ps-shop--category">
         <div class="ps-container-fluid">
             <div class="masonry-wrapper" data-col-md="5" data-col-sm="3" data-col-xs="1" data-gap="30" data-radio="4:3">
@@ -20,104 +22,113 @@
                         <div class="grid-item__content-wrapper">
                             <div class="ps-collection--bottom"><a class="ps-collection__morelink"
                                                                   href="#">Диваны</a><img
-                                    class="ps-fullwidth" src="assets/images/shop/category-1.jpg" alt=""></div>
+                                    class="ps-fullwidth" src="assets/images/shop/category-1.jpg" alt="Диваны"></div>
                         </div>
                     </div>
                     <div class="grid-item">
                         <div class="grid-item__content-wrapper">
                             <div class="ps-collection--bottom"><a class="ps-collection__morelink"
                                                                   href="#">Кровати</a><img
-                                    class="ps-fullwidth" src="assets/images/shop/category-2.jpg" alt=""></div>
+                                    class="ps-fullwidth" src="assets/images/shop/category-2.jpg" alt="Кровати"></div>
                         </div>
                     </div>
                     <div class="grid-item">
                         <div class="grid-item__content-wrapper">
                             <div class="ps-collection--bottom"><a class="ps-collection__morelink" href="#">Пуфы</a><img
-                                    class="ps-fullwidth" src="assets/images/shop/category-3.jpg" alt=""></div>
+                                    class="ps-fullwidth" src="assets/images/shop/category-3.jpg" alt="Пуфы"></div>
                         </div>
                     </div>
                     <div class="grid-item">
                         <div class="grid-item__content-wrapper">
                             <div class="ps-collection--bottom"><a class="ps-collection__morelink"
                                                                   href="#">Кушетки</a><img
-                                    class="ps-fullwidth" src="assets/images/shop/category-4.jpg" alt=""></div>
+                                    class="ps-fullwidth" src="assets/images/shop/category-4.jpg" alt="Кушетки"></div>
                         </div>
                     </div>
                     <div class="grid-item">
                         <div class="grid-item__content-wrapper">
-                            <div class="ps-collection--bottom"><a class="ps-collection__morelink" href="#">Мягкие
-                                    настенные
-                                    панели</a><img class="ps-fullwidth" src="assets/images/shop/category-5.jpg" alt="">
+                            <div class="ps-collection--bottom">
+                                <a class="ps-collection__morelink" href="#">Мягкие настенные панели</a>
+                                <img class="ps-fullwidth" src="assets/images/shop/category-5.jpg"
+                                     alt="Мягкие настенные панели">
                             </div>
                         </div>
                     </div>
                     <div class="grid-item">
                         <div class="grid-item__content-wrapper">
-                            <div class="ps-collection--bottom"><a class="ps-collection__morelink" href="#">Шумоизоляционные
-                                    панели мягкие</a><img class="ps-fullwidth" src="assets/images/shop/category-6.jpg"
-                                                          alt=""></div>
+                            <div class="ps-collection--bottom">
+                                <a class="ps-collection__morelink" href="#">Шумоизоляционные панели мягкие</a>
+                                <img class="ps-fullwidth" src="assets/images/shop/category-6.jpg"
+                                     alt="Шумоизоляционные панели мягкие"></div>
                         </div>
                     </div>
                     <div class="grid-item">
                         <div class="grid-item__content-wrapper">
-                            <div class="ps-collection--bottom"><a class="ps-collection__morelink" href="#">Офисные
-                                    диваны</a><img class="ps-fullwidth" src="assets/images/shop/category-7.jpg" alt="">
+                            <div class="ps-collection--bottom">
+                                <a class="ps-collection__morelink" href="#">Офисные диваны</a>
+                                <img class="ps-fullwidth" src="assets/images/shop/category-7.jpg" alt="Офисные диваны">
                             </div>
                         </div>
                     </div>
                     <div class="grid-item">
                         <div class="grid-item__content-wrapper">
-                            <div class="ps-collection--bottom"><a class="ps-collection__morelink"
-                                                                  href="#">Кресла</a><img
-                                    class="ps-fullwidth" src="assets/images/shop/category-12.jpg" alt=""></div>
+                            <div class="ps-collection--bottom">
+                                <a class="ps-collection__morelink" href="#">Кресла</a>
+                                <img class="ps-fullwidth" src="assets/images/shop/category-12.jpg" alt="Кресла">
+                            </div>
                         </div>
                     </div>
                     <div class="grid-item large">
                         <div class="grid-item__content-wrapper">
-                            <div class="ps-collection--bottom"><a class="ps-collection__morelink" href="#">Коллекция
-                                    2024</a><img class="ps-fullwidth" src="assets/images/shop/category-11.jpg" alt="">
+                            <div class="ps-collection--bottom">
+                                <a class="ps-collection__morelink" href="#">Коллекция 2024</a>
+                                <img class="ps-fullwidth" src="assets/images/shop/category-11.jpg" alt="Коллекция 2024">
                             </div>
                         </div>
                     </div>
                     <div class="grid-item">
                         <div class="grid-item__content-wrapper">
-                            <div class="ps-collection--bottom"><a class="ps-collection__morelink" href="#">Изголовья
-                                    кровати</a><img class="ps-fullwidth" src="assets/images/shop/category-13.jpg"
-                                                    alt="">
+                            <div class="ps-collection--bottom">
+                                <a class="ps-collection__morelink" href="#">Изголовья кровати</a>
+                                <img class="ps-fullwidth" src="assets/images/shop/category-13.jpg"
+                                     alt="Изголовья кровати">
                             </div>
                         </div>
                     </div>
                     <div class="grid-item">
                         <div class="grid-item__content-wrapper">
-                            <div class="ps-collection--bottom"><a class="ps-collection__morelink" href="#">Мягкие панели
-                                    декор</a><img class="ps-fullwidth" src="assets/images/shop/category-15.jpg" alt="">
+                            <div class="ps-collection--bottom">
+                                <a class="ps-collection__morelink" href="#">Мягкие панели декор</a>
+                                <img class="ps-fullwidth" src="assets/images/shop/category-15.jpg"
+                                     alt="Мягкие панели декор">
                             </div>
                         </div>
                     </div>
                     <div class="grid-item">
                         <div class="grid-item__content-wrapper">
-                            <div class="ps-collection--bottom"><a class="ps-collection__morelink"
-                                                                  href="#">Банкетки</a><img
-                                    class="ps-fullwidth" src="assets/images/shop/category-8.jpg" alt=""></div>
+                            <div class="ps-collection--bottom">
+                                <a class="ps-collection__morelink" href="#">Банкетки</a><img
+                                    class="ps-fullwidth" src="assets/images/shop/category-8.jpg" alt="Банкетки"></div>
                         </div>
                     </div>
                     <div class="grid-item">
                         <div class="grid-item__content-wrapper">
-                            <div class="ps-collection--bottom"><a class="ps-collection__morelink"
-                                                                  href="#">Матрасы</a><img
-                                    class="ps-fullwidth" src="assets/images/shop/category-9.jpg" alt=""></div>
+                            <div class="ps-collection--bottom">
+                                <a class="ps-collection__morelink" href="#">Матрасы</a>
+                                <img class="ps-fullwidth" src="assets/images/shop/category-9.jpg" alt="Матрасы">
+                            </div>
                         </div>
                     </div>
                     <div class="grid-item">
                         <div class="grid-item__content-wrapper">
-                            <div class="ps-collection--bottom"><a class="ps-collection__morelink"
-                                                                  href="#">Танкетки</a><img
-                                    class="ps-fullwidth" src="assets/images/shop/category-10.jpg" alt=""></div>
+                            <div class="ps-collection--bottom">
+                                <a class="ps-collection__morelink" href="#">Танкетки</a>
+                                <img class="ps-fullwidth" src="assets/images/shop/category-10.jpg" alt="Танкетки">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <!--include ../../partials/section/subscribe-2-->
 @endsection
