d6c0a3e4c246852b1926241332ddfe04955949e7
Shokhrukh <shohruhhon@gmail.com>
2024-12-26 19:07:43 +0500

    fix telegram bot

--- app/Http/Controllers/CallbackController.php
+++ app/Http/Controllers/CallbackController.php
@@ -46,6 +46,8 @@ class CallbackController extends Controller
             'text' => $message,
             'parse_mode' => 'HTML',
         ]);
+        // Логирование ответа от Telegram
+        \Log::info('Telegram API response', ['response' => $response->body()]);
 
         if ($response->failed()) {
             throw new \Exception('Не удалось отправить сообщение в Telegram.');
